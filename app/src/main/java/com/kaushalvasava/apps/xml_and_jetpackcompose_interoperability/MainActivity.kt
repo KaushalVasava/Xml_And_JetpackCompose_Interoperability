36df0d6d9950d2bb317f09de8cc36cdacd160226
KaushalVasava <vasavakaushal333@gmail.com>
2023-09-01 13:07:17 +0530

    Add BarCodeView inside Jetpack compose

--- app/src/main/java/com/kaushalvasava/apps/xml_and_jetpackcompose_interoperability/MainActivity.kt
+++ app/src/main/java/com/kaushalvasava/apps/xml_and_jetpackcompose_interoperability/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.kaushalvasava.apps.xml_and_jetpackcompose_interoperability
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.kaushalvasava.apps.xml_and_jetpackcompose_interoperability.ui.screen.ScanScreen
 import com.kaushalvasava.apps.xml_and_jetpackcompose_interoperability.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +27,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            RequestPermission()
             ComposeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                   ScanScreen()
                 }
             }
         }
@@ -31,12 +42,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
-    Text("Hello Compose ")
-}
+fun RequestPermission() {
+    val launcher: ManagedActivityResultLauncher<String, Boolean> =
+        rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission Accepted: Do something
+                Log.d("TAG", "PERMISSION GRANTED")
+//                startService(intent)
+//                bindService(intent, connection, Context.BIND_AUTO_CREATE)
+            }
+        }
 
-@Preview
-@Composable
-fun GreetingPreview() {
-    Greeting()
+    when (PackageManager.PERMISSION_GRANTED) {
+        ContextCompat.checkSelfPermission(
+            LocalContext.current,
+            android.Manifest.permission.CAMERA
+        ) -> {
+            // Some works that require permission
+            Log.d("TAG", "Code requires permission")
+        }
+
+        else -> {
+            // Asking for permission
+            SideEffect {
+                launcher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
 }
